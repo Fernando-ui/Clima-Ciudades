b72618015c66f4cf86b2af16c751dac1af2a57a3
fersan <fst-96@hotmail.com>
2021-04-14 10:46:14 -0500

    Agregando las busquedas

--- models/busquedas.js
+++ models/busquedas.js
@@ -11,14 +11,29 @@ class Busquedas {
         //TODO: Leer el db 
 
     }
+    get getParamsMapbox ( ){
+        return{
+            
+                'access_token': 'pk.eyJ1IjoiZmVyc3QiLCJhIjoiY2tuaGwyeHdpMG1vajJvcGNjbW02andyaiJ9.xukQtlq_0-WccfDnaD_fnA',
+                'limit': 5,
+                'languaje':'es'
+            
+        }
+    };
 
     async ciudad ( lugar = ''){
 
         try{
             // Peticon http
-            // console.log('ciudad',lugar);
-            const resp = await axios.get('https://reppppqres.in/api/users?page=2');
-            console.log(resp.data.total);
+            
+            const instance = axios.create({
+                baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.getParamsMapbox
+            });
+
+            const resp = await instance.get();
+
+            console.log(resp.data);
     
     
             return [];
